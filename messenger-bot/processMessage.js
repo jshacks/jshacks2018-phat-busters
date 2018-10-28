6e2dd36fdf05a468d14d5a5dae5e4a1238521465
Adrian Oprea <adrian@oprea.rocks>
2018-10-28 15:25:49 +0200

    Feature/message processing module (#7)
    * feature: Add message processing module
    
    * refactor: Throw an error when wit.ai calls fail
    
    Error should be catched at a superior level and a canned message must be sent to the user.

---
+++ messenger-bot/processMessage.js
@@ -0,0 +1,21 @@
+const witClient = require('./wit-ai')
+
+const processMessage = async (event) => {
+  const senderPSID = event.sender.id
+  const {text, attachments} = event.message;
+
+  try {
+    const result = await witClient.message(text)
+    if (result) {
+      return result;
+    }
+  } catch (error) {
+    console.log(`Wit.ai call error`)
+    throw new Error({
+      message: `Wit.ai call error`,
+      error,
+    })
+  }
+}
+
+module.exports = processMessage;
